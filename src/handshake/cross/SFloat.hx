d4ea2f7bee569b46cdaf1f3f7a53710d36512eb8
shinji-yoshida <yoshida.shinji.gm@gmail.com>
2014-08-31 00:43:49 +0900

    rewrite SFloat

--- src/handshake/cross/SFloat.hx
+++ src/handshake/cross/SFloat.hx
@@ -1,36 +1,7 @@
 package handshake.cross;
 
-abstract SFloat(Single) from Single to Single to Float {
-  inline public function new(s:Single) {
-    this = s;
-  }
-
-  @:from
-  inline public static function fromInt(i:Int):SFloat {
-    return new SFloat(i);
-  }
-
-  @:op(A + B)
-  inline public function plus(rhs:SFloat):SFloat {
-    return new SFloat(this + rhs.get());
-  }
-
-  @:op(A - B)
-  inline public function minus(rhs:SFloat):SFloat {
-    return new SFloat(this - rhs.get());
-  }
-
-  @:op(A * B)
-  inline public function mul(rhs:SFloat):SFloat {
-    return new SFloat(this * rhs.get());
-  }
-
-  @:op(A / B)
-  inline public function divide(rhs:SFloat):SFloat {
-    return new SFloat(this / rhs.get());
-  }
-
-  inline public function get():Single{
-    return this;
-  }
-}
+#if cs
+typedef SFloat=Single;
+#else
+typedef SFloat=Float;
+#end
